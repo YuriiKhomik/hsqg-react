223164a1b8e94f3f02076f2f4e95a8c5bdbbf1c7
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-06-27 12:24:37 +0300

    added basename to Browser Router

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { ThemeProvider } from 'styled-components';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="hsqg-react">
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
