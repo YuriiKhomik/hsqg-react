206f451898bdaa1abb9ed824914239de9923c4cb
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-07-04 17:03:14 +0300

    ...

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -8,16 +8,13 @@ export const StyledMenu = styled.nav`
   justify-content: center;
   backdrop-filter: blur(10px);
   height: 100%;
+  width: 100%;
   text-align: left;
   position: absolute;
   top: 0;
   left: 0;
   transition: transform 0.3s ease-in-out;
   transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
-
-  @media (max-width: ${breakpoints.tablet}) {
-    width: 100%;
-  }
 `;
 
 export const StyledLink = styled(NavLink)`
@@ -27,13 +24,10 @@ export const StyledLink = styled(NavLink)`
   text-transform: uppercase;
   color: ${p => p.theme.colors.white};
 
-  -webkit-text-fill-color: ${p => p.theme.colors.white};
-  -webkit-text-stroke-width: 0.7px;
-  -webkit-text-stroke-color: ${p => p.theme.colors.black};
-  -webkit-text-stroke: 0.7px ${p => p.theme.colors.black};
-  text-fill-color: ${p => p.theme.colors.white};
-  text-stroke-width: 0.7px;
-  text-stroke-color: ${p => p.theme.colors.black};
+  text-shadow: 0.7px 0px ${p => p.theme.colors.black},
+    0px 0.7px ${p => p.theme.colors.black},
+    -0.7px 0px ${p => p.theme.colors.black},
+    0px -0.7px ${p => p.theme.colors.black};
 
   margin: 0 auto 16px;
   padding: 12px 16px;
